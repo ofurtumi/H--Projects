5c6caa811d9738082962ff0121fbdbeeb7931b8c
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2021-09-12 20:18:39 +0000

    Tímadæmi 3 og skiladæmi 3

---
+++ Töl-101/Tim 3/Positive2.java
@@ -0,0 +1,36 @@
+/**
+ * @ Höfundur: 	Þorvaldur Tumi Baldursson
+ * @ Netfang: 	ttb3@hi.is
+ * @ Búið til: 	2021-12-09 11:21:56
+ * @ Lýsing: 	Tekur inn tvær tölur af skipanalínu og segir til hvor þær séu
+ *              báðar jákvæðar, önnur jákvæð eða hvorug jákvæð
+ */
+
+public class Positive2 {
+    public static void main(String[] args) {
+
+
+        // Það sem forritið á ekki að gera en ég hélt að ætti að gera alla helgina
+        //
+        // int a = Integer.parseInt(args[0])%2;
+        // int b = Integer.parseInt(args[1])%2;
+
+        // int c = a + b;
+        // switch (c) {
+        //     case 0:
+        //         System.out.println("Báðar jákvæðar");
+        //         break;
+        
+        //     case 1:
+        //         System.out.println("Önnur jákvæð");
+        //         break;
+        
+        //     case 2:
+        //         System.out.println("Hvorug jákvæð");
+        //         break;
+                
+        //     default:
+        //         break;
+        // }
+    }
+}
