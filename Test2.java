e14d1a84c41e0d086f20e9d13724ddc3fac2b148
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2021-09-24 11:00:05 +0000

    Gitignore add og test

--- Test2.java
+++ Test2.java
@@ -1,6 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
-        double c = StdRandom.uniform(0.0, 1.0);
-        System.out.println(c);
+        int[][] a = new int[10][];
+        System.out.println(a[1]);
     }
 }
