c827c53636037098274f3f2dfad417e1c3bbc97c
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2021-10-01 15:18:37 +0000

    Teiknir

--- Test.java
+++ Test.java
@@ -1,30 +1,36 @@
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
+        StdDraw.enableDoubleBuffering();
         StdDraw.setScale(-10,10);
         double x1 = 11.0;
         double x2 = 11.0;
         double y1 = 11.0;
         double y2 = 11.0;
+
+        ArrayList<Double> hnit = new ArrayList<Double>();
         while (true) {
             if (StdDraw.isKeyPressed(KeyEvent.VK_1)) {
                 x1 = StdDraw.mouseX();
                 y1 = StdDraw.mouseY();
-            }
-            if (StdDraw.isKeyPressed(KeyEvent.VK_2)) {
-                x2 = StdDraw.mouseX();
-                y2 = StdDraw.mouseY();
 
-                if (x1 <= 10.0 && x2 <= 10 && y1 <= 10.0 && y2 <= 10) {
+                while (!StdDraw.isKeyPressed(KeyEvent.VK_2)) {
+                    x2 = StdDraw.mouseX();
+                    y2 = StdDraw.mouseY();
+                    StdDraw.clear();
+                    for (int i = 0; i<hnit.size(); i=i+4) {
+                        StdDraw.line(hnit.get(i), hnit.get(i+1), hnit.get(i+2), hnit.get(i+3));
+                    }
                     StdDraw.line(x1, y1, x2, y2);
-                    x1 = 11.0;
-                    x2 = 11.0;
-                    y1 = 11.0;
-                    y2 = 11.0;
+                    StdDraw.show();
                 }
+                hnit.add(x1);
+                hnit.add(y1);
+                hnit.add(x2);
+                hnit.add(y2);
             }
-
         }
     }
 }
