ad5842cc954343785742b07dc39f072102bee367
Tumi <thorvaldur.tumi@gmail.com>
2021-11-01 12:34:47 +0000

    Skil 10 4

--- Töl-101/Skil 10/Leit.java
+++ Töl-101/Skil 10/Leit.java
@@ -1,23 +1,36 @@
+/**
+ * @ Höfundur: 	Þorvaldur Tumi Baldursson
+ * @ Netfang: 	ttb3@hi.is
+ * @ Búið til: 	2021-01-11 11:22
+ * @ Lýsing: 	Inniheldur tvö föll, main til þess að testa, og leit sem hefur sína eigin javadoc lýsingu
+ */
+
 public class Leit {
     public static void main(String[] args) {
         System.out.println(leit("nemendur.txt", "@"));
-        System.out.println(leit("nemendur.txt", "dóttir"));
+        System.out.println(leit("nemendur.txt", "dóttir")); // virðist vera ehv vesen með kommustafi
         System.out.println(leit("nemendur.txt", "Reykjavík"));
         System.out.println(leit("nemendur.txt", "son"));
-        In s = new In("nemendur.txt");
-        System.out.println(s.readLine());
     }
 
+    /**
+     * Tekur inn tvo strengi og leitað að seinni strengnum í hverri línu af skrá með nafn fyrri strengsins
+     * @param nafnASkra nafn á skrá sem á að leita í
+     * @param ord strengur sem leita á að í nafnASkra skránni
+     * @return heiltölu sem táknar fjölda lína í skrá sem innihalda orðið sem leitað var að
+     */
     public static int leit(String nafnASkra, String ord) {
         In s = new In(nafnASkra);
         int fjoldiOrda = 0;
         
         while(s.hasNextLine()) {
             String lina = s.readLine();
-            if (lina.contains(lina)) {
+            if (lina.contains(ord)) {
                 fjoldiOrda++;
             }
         }
+
+        s.close();
 
         return fjoldiOrda;
     }
