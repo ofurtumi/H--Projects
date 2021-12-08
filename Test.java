e68c41965e8f71071a01800a98482deec9b65f43
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2021-12-08 10:57:00 +0000

    hvernig lookar?

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println((int)10.25+7.98);
-        System.out.println((int)10.25+(int)7.98);
-        System.out.println((int)(10.25+7.98));
+        switch (key) {
+            case value:
+                function();
+                break;
+        
+            default:
+                break;
+        }
     }
 }
