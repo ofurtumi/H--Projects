cf4cde979d82fff70a080abafe07984fb5e9fe74
Tumi <thorvaldur.tumi@gmail.com>
2022-02-09 10:33:48 +0000

    v4 aint got nothing on me!

--- tolv-2/v4/Triplicate.java
+++ tolv-2/v4/Triplicate.java
@@ -1,12 +1,38 @@
-public class Triplicate{
-    public static String triplicate(String[] a,String[] b,String[] c){
-        for (String oa : a) {
-            for (String ob : b) {
-                if (oa.equals(ob)) {
-                    for (String oc : c) {
-                        if (oa.equals(oc)) return oa;
+import edu.princeton.cs.algs4.Merge;
+
+public class Triplicate {
+    public static String triplicate(String[] a, String[] b, String[] c) {
+        Merge.sort(a);
+        Merge.sort(b);
+        Merge.sort(c);
+
+        for (int i = 0; i < a.length; i++) {
+            int lo = 0;
+            int hi = b.length - 1;
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+                if (a[i].compareTo(b[mid]) < 0)
+                    hi = mid - 1;
+                else if (a[i].compareTo(b[mid]) > 0)
+                    lo = mid + 1;
+                else {
+                    int l = 0;
+                    int h = c.length - 1;
+                    while (l <= h) {
+                        int m = (l + h) / 2;
+                        if (a[i].compareTo(c[m]) < 0) {
+                            h = m - 1;
+                        }
+                        else if (a[i].compareTo(c[m]) > 0) {
+                            l = m + 1;
+                        }
+                        else {
+                            return a[i];
+                        }
                     }
+                    break;
                 }
+
             }
         }
 
@@ -14,10 +40,13 @@ public class Triplicate{
     }
 
     public static void main(String[] args) {
-        String[] a = {"oli","joi","stebbi","stina"};
-        String[] b = {"jenny","stina","gunna","loa"};
-        String[] c = {"hall","tumi","gulli","stina"};
+        String[] a = { "oli", "joi", "stebbi", "stjani" };
+        String[] b = { "jenny", "stjani", "loa" };
+        String[] c = { "aaa" };
 
+        // Stopwatch s1 = new Stopwatch();
         System.out.println(triplicate(a, b, c));
+        // double t1 = s1.elapsedTime();
+        // System.out.println(t1);
     }
 }
