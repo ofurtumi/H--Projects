1ebc03c50747da1d3e5a49c133f4423091b5feeb
Tumi <thorvaldur.tumi@gmail.com>
2022-02-24 17:25:26 +0000

    gera þetta sætt og fint

--- tolv-2/v6/MedianFinder.java
+++ tolv-2/v6/MedianFinder.java
@@ -5,39 +5,42 @@ public class MedianFinder<Key extends Comparable<Key>> {
     private MinPQ<Key> minpq;
 
     public MedianFinder() {
-        //búið til maxpq og minpx
-        maxpq = new MaxPQ<Key>(); //geymir stærri helming raðarinnar??
-        minpq = new MinPQ<Key>(); //geymir minni helming raðarinnar???
+        // búið bæði trén
+        maxpq = new MaxPQ<Key>(); // geymir stök frá miðju og niður
+        minpq = new MinPQ<Key>(); // geymir stök frá miðju og upp
     }
 
     public Key findMedian() {
         int maxSize = maxpq.size();
         int minSize = minpq.size();
 
-        if (maxpq.isEmpty() && minpq.isEmpty()) return null;
-        else if (maxSize == minSize || maxSize > minSize) return maxpq.max();
-        else return minpq.min();
+        if (maxpq.isEmpty() && minpq.isEmpty()) return null; // kemur í veg fyrir stack underflow
+        else if (maxSize == minSize || maxSize > minSize) return maxpq.max(); // skilar miðjunni, rót í vinstra trénu
+        else return minpq.min(); // skilar miðjunni, rót í hægra trénu
     }
 
     public Key deleteMedian() {
         int maxSize = maxpq.size();
         int minSize = minpq.size();
-        Key deleted = null;
+        Key deleted = null; // frumstilla skilagildið
 
-        if (maxpq.isEmpty() && minpq.isEmpty()) return null;
+        if (maxpq.isEmpty() && minpq.isEmpty()) return null; // kemur í veg fyrir stack underflow
         else if (maxpq.isEmpty() || minpq.isEmpty()) {
             if (maxpq.isEmpty()) deleted = minpq.delMin();
             else deleted = maxpq.delMax();
         }
-        else if (maxSize == minSize || maxSize > minSize) deleted = maxpq.delMax();
-        else deleted = minpq.delMin();
+        else if (maxSize == minSize || maxSize > minSize) deleted = maxpq.delMax(); // eyðir miðju, rót í vinstra trénu
+        else deleted = minpq.delMin(); // eyðir miðju, rót í hægra trénu
         return deleted;
     }
 
     public void insert(Key key) {
-        if (maxpq.isEmpty() || key.compareTo(maxpq.max()) < 0) maxpq.insert(key);
-        else minpq.insert(key);
+        if (maxpq.isEmpty() || key.compareTo(maxpq.max()) < 0) maxpq.insert(key); // setur inn í vinstra tréð ef key er minna en rótin
+        else minpq.insert(key); // setur inn í hægra tréð
         
+        // passar að hæðarmunur á trjánum verði aldrei meiri en 1
+        // ef hæðarmunur er meiri en einn, taka rót stærra trésins og setja í minna tréð
+
         if (maxpq.size() > minpq.size()+1) minpq.insert(maxpq.delMax());
         else if (minpq.size() > maxpq.size()+1) maxpq.insert(minpq.delMin());
     }
@@ -50,7 +53,6 @@ public class MedianFinder<Key extends Comparable<Key>> {
         m.insert("C");
         m.insert("D");
         m.insert("E");
-        // print();
 
         System.out.println(m.findMedian());
         m.deleteMedian();
@@ -63,8 +65,5 @@ public class MedianFinder<Key extends Comparable<Key>> {
         System.out.println(m.findMedian());
         m.deleteMedian();
         System.out.println(m.findMedian());
-
     }
-
-    // nota maxpq og minpq
 }
